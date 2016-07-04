2af14ac00199cb9dc9aea0538671d6f912469c65
DennisDaddy <softteez95@gmail.com>
2016-07-04 23:33:26 +0300

    more variables and printing

---
+++ ex5.rb
@@ -0,0 +1,19 @@
+my_name = 'Zed A. Shaw'
+my_age = 35
+my_height = 74
+my_weight = 180
+my_eyes = 'Blue'
+my_teeth = "white"
+my_hair = "Brown"
+
+puts "lets talk about #{my_name}"
+puts " He's #{my_height} inches tall"
+puts " He's #{my_weight} pounds heavy."
+puts "Actually that's not too heavy."
+puts "He's got  #{my_eyes} eyes and #{my_hair} hair" 
+puts "His teeth are usual #{my_teeth} depending on the cofee."
+
+puts "f i had  #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}"
+
+puts "lets talk about #{my_name}"
+
