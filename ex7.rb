12e3cbc207db498396bb1547ac9ed8a166b5a28a
DennisDaddy <softteez95@gmail.com>
2016-07-05 15:09:03 +0300

    more printing

---
+++ ex7.rb
@@ -0,0 +1,14 @@
+puts "Mary had a little lamb."
+puts "Its fleece was white as #{'snow'}."
+puts "And everywhere that mary went."
+puts "." * 10
+end1  = "C"
+end2 = "B"
+end3 = "B"
+end4 = "B"
+end5 = "u"
+end6 = "r"
+end7 ="g"
+end8 = "r"
+
+puts end7 + end8 + end9 + end10 + end11 + end12
